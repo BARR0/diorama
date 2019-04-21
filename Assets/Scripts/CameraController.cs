c537bced47d910165370133fc79f99726c45a08b
Andres Barro <andresbarroe@gmail.com>
2019-04-21 14:25:44 -0500

    class
    Signed-off-by: Andres Barro <andresbarroe@gmail.com>

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // [SerializedField] Transform camera;
+    public Transform camera;
+    public Transform cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // camera.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera.position = Vector3.MoveTowards(camera.position, cube.position, Time.deltaTime);
+    }
+}

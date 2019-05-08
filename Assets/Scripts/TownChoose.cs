cbc5bc46b4f3c7c2b92fe09265943886f5c1fd7f
Andres Barro <andresbarroe+git@gmail.com>
2019-05-08 10:49:00 -0500

    animations
    Signed-off-by: Andres Barro <andresbarroe+git@gmail.com>

---
+++ Assets/Scripts/TownChoose.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownChoose : MonoBehaviour
+{
+    public Animator animator;
+    private bool good;
+    // Start is called before the first frame update
+    void Start()
+    {
+        good = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        animator.SetBool("good", good);
+    }
+}

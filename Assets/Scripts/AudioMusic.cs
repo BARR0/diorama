5ee6b5b73bd8d9a204796295bfb810cdfa4984ac
Andres Barro <andresbarroe+git@gmail.com>
2019-05-10 01:36:52 -0500

    Entrega
    Signed-off-by: Andres Barro <andresbarroe+git@gmail.com>

---
+++ Assets/Scripts/AudioMusic.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioMusic : MonoBehaviour
+{
+    public AudioClip otherClip;
+    private AudioSource audio;
+
+    // IEnumerator Start()
+    // {
+    //     audio = GetComponent<AudioSource>();
+
+    //     audio.Play();
+    //     yield return new WaitForSeconds(audio.clip.length);
+    //     audio.clip = otherClip;
+    //     audio.Play();
+    // }
+
+    // public static IEnumerator FadeOut (AudioSource audioSource, float FadeTime) {
+    //     float startVolume = audioSource.volume;
+ 
+    //     while (audioSource.volume > 0) {
+    //         audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+ 
+    //         yield return null;
+    //     }
+ 
+    //     audioSource.Stop ();
+    //     audioSource.volume = startVolume;
+    // }
+
+    // public void FadeMain() {
+    //     audio = GetComponent<AudioSource>();
+    //     StartCoroutine (FadeOut (audio, 1.0f));
+    // }
+
+    // public void PlayGoodMusic() {
+    //     audio = GetComponent<AudioSource>();
+    //     audio.clip = goodClip;
+    //     audioSource.volume = 1.0;
+    //     audio.Play();
+    // }
+
+    public void PlayGongBad() {
+        audio = GetComponent<AudioSource>();
+        // audio.clip = otherClip;
+        // audio.volume = 1.0f;
+        audio.Play();
+    }
+}
--- Assets/Scripts/TownChoose.cs
+++ Assets/Scripts/TownChoose.cs
@@ -1,21 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TownChoose : MonoBehaviour
 {
     public Animator animator;
-    private bool good;
+    // private bool good;
+    public Canvas canvas;
+    public Button bg, be;
+
+    public AudioClip otherClip, goodClip, badClip;
+    private AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
-        good = false;
+        // good = true;
+        canvas.gameObject.SetActive(false);
+        bg.onClick.AddListener(SetGood);
+        be.onClick.AddListener(SetEvil);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        animator.SetBool("good", good);
+        // animator.SetBool("good", good);
+    }
+
+    public void ShowButtons () {
+        canvas.gameObject.SetActive(true);
+    }
+
+    public void HideButtons () {
+        canvas.gameObject.SetActive(false);
+    }
+
+    public void SetGood () {
+        animator.SetTrigger("good");
+        PlayGoodMusic();
+    }
+
+    public void SetEvil () {
+        animator.SetTrigger("evil");
+        PlayEvilMusic();
+    }
+
+    // IEnumerator Start()
+    // {
+    //     audio = GetComponent<AudioSource>();
+
+    //     audio.Play();
+    //     yield return new WaitForSeconds(audio.clip.length);
+    //     audio.clip = otherClip;
+    //     audio.Play();
+    // }
+
+    public static IEnumerator FadeOut (AudioSource audioSource, float FadeTime) {
+        float startVolume = audioSource.volume;
+ 
+        while (audioSource.volume > 0) {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+ 
+            yield return null;
+        }
+ 
+        audioSource.Stop ();
+        audioSource.volume = startVolume;
+    }
+
+    public void FadeMain() {
+        audio = GetComponent<AudioSource>();
+        StartCoroutine (FadeOut (audio, 2.0f));
+    }
+
+    public void PlayGoodMusic() {
+        audio = GetComponent<AudioSource>();
+        audio.clip = goodClip;
+        audio.volume = 1.0f;
+        audio.Play();
+    }
+
+    public void PlayEvilMusic() {
+        audio = GetComponent<AudioSource>();
+        audio.clip = badClip;
+        audio.volume = 1.0f;
+        audio.Play();
+    }
+
+    public void PlayGong() {
+        audio = GetComponent<AudioSource>();
+        audio.clip = otherClip;
+        audio.volume = 1.0f;
+        audio.Play();
     }
 }
